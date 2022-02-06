b7cb3ebd89fbbfab47033fec8713a6c292962b52
Bruno Monte <brunoaguiarprofissional@gmail.com>
2022-02-05 23:23:01 -0300

    name database

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/autenticacao') // forma de conectar ao mongo
+mongoose.connect('mongodb://localhost:27017/autenticação')// forma de conectar ao mongo
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
