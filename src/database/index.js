113c13357c41e886141af6ffeeefcbc0256a16ce
Bruno Monte <brunoaguiarprofissional@gmail.com>
2022-02-06 22:26:40 -0300

    tratamento error db

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/autenticação')// forma de conectar ao mongo
+catch(error => handlerError(error))
+try {
+    await mongoose.connect('mongodb://localhost:27017/autenticação')
+} catch (error) {
+   handlerError(error) 
+}
+
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
