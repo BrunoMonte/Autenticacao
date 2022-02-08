e40d23cf024ed4ca741a815845bb70a4e8a88c4b
Bruno Monte <brunoaguiarprofissional@gmail.com>
2022-02-07 22:42:41 -0300

    adc services

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/autenticação')// forma de conectar ao mongo
-catch(error => handlerError(error))
+
 try {
     await mongoose.connect('mongodb://localhost:27017/autenticação')
 } catch (error) {
